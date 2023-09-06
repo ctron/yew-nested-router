875de4eff2132500c758b771fcecd39cc6e5108e
Jens Reimann <ctron@dentrassi.de>
2023-09-06 09:36:07 +0200

    chore: don't log too much

--- src/base.rs
+++ src/base.rs
@@ -10,7 +10,7 @@ pub(crate) fn eval_base() -> Option<String> {
     if let Ok(Some(base)) = gloo_utils::document().query_selector("base[href]") {
         let base = extract_base(base.unchecked_into::<web_sys::HtmlBaseElement>().href());
 
-        log::info!("Base: {base:?}");
+        // log::info!("Base: {base:?}");
         return base;
     }
 

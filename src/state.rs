123cb815d02cfb4a90bcdc3ba6517fed0ccd5378
Jens Reimann <ctron@dentrassi.de>
2023-12-19 15:33:05 +0100

    chore: a bit more convenience for handling `State`

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,24 @@
+use serde::Serialize;
 use wasm_bindgen::JsValue;
 use yew::html::IntoPropValue;
 
 /// A page state value
+///
+/// This is a thing wrapper around [`JsValue`], allowing for an easier interaction with the API,
+/// especially in the context of `yew`.
 #[derive(PartialEq, Debug, Clone)]
 pub struct State(pub(crate) JsValue);
 
 impl State {
+    /// A `null` value
     pub const fn null() -> Self {
         State(JsValue::null())
+    }
+
+    /// Serialize a value into [`JsValue`].
+    pub fn json<S: Serialize>(value: &S) -> Result<Self, serde_json::Error> {
+        use gloo_utils::format::JsValueSerdeExt;
+        Ok(State(JsValue::from_serde(value)?))
     }
 }
 
@@ -26,5 +37,23 @@ impl Default for State {
 impl IntoPropValue<State> for &str {
     fn into_prop_value(self) -> State {
         State(JsValue::from_str(self))
+    }
+}
+
+impl IntoPropValue<State> for String {
+    fn into_prop_value(self) -> State {
+        State(JsValue::from_str(&self))
+    }
+}
+
+impl IntoPropValue<State> for &String {
+    fn into_prop_value(self) -> State {
+        State(JsValue::from_str(&self))
+    }
+}
+
+impl IntoPropValue<State> for JsValue {
+    fn into_prop_value(self) -> State {
+        State(self)
     }
 }
